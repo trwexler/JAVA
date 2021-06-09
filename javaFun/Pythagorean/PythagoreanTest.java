2a1ac4cbaa23ac16dbd5c0c6c3aa32c5c5ded906
Tom Wexler <wexler18@gmail.com>
2021-06-09 10:13:31 -0400

    java practice

---
+++ javaFun/Pythagorean/PythagoreanTest.java
@@ -0,0 +1,7 @@
+public class PythagoreanTest {
+	public static void main(String[] args){
+		Pythagorean iD = new Pythagorean();
+		double hypotenuse = iD.calculateHypotenuse(4, 5);
+		System.out.println(hypotenuse);
+	}
+}
