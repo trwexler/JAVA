6a1772195969f3b09a52d0918f9a163acc8d13ac
Tom Wexler <wexler18@gmail.com>
2021-06-10 10:00:05 -0400

    StringManipulation Complete

---
+++ javaFun/StringManipulation/StringManipulator.java
@@ -0,0 +1,32 @@
+public class StringManipulator {
+
+    public String trimAndConcat(String str1, String str2) {
+        return str1.trim().concat(str2.trim());
+    }
+
+    public Integer getIndexOrNull(String str1, char z) {
+        if(str1.indexOf(z) == -1){
+            return null;
+        }
+        else{
+            return str1.indexOf(z);
+        }
+    
+    }
+
+    public Integer getIndexOrNull2(String str1, String str2) {
+        if(str1.indexOf(str2) == -1){
+            return null;
+        }
+        else{
+            return str1.indexOf(str2);
+        }
+    
+    }
+
+    public String concatSubstring(String str1, int start, int end, String str2) {
+        str1 = str1.substring(start, end);
+        return str1.concat(str2);
+    }
+}
+
---
+++ javaFun/StringManipulation/StringManipulatorTest.java
@@ -0,0 +1,33 @@
+public class StringManipulatorTest {
+	public static void main(String[] args){
+
+		StringManipulator manipulator = new StringManipulator();
+		String str = manipulator.trimAndConcat("    Hello     ","     World    ");
+		System.out.println(str); // HelloWorld 
+
+		StringManipulator manipulator = new StringManipulator();
+		char letter = 'o';
+		Integer a = manipulator.getIndexOrNull("Coding", letter);
+		Integer b = manipulator.getIndexOrNull("Hello World", letter);
+		Integer c = manipulator.getIndexOrNull("Hi", letter);
+		System.out.println(a); // 1
+		System.out.println(b); // 4
+		System.out.println(c); // null
+
+		StringManipulator manipulator = new StringManipulator();
+		String word = "Hello";
+		String subString = "llo";
+		String notSubString = "world";
+		Integer a = manipulator.getIndexOrNull2(word, subString);
+		Integer b = manipulator.getIndexOrNull2(word, notSubString);
+		System.out.println(a); // 2
+		System.out.println(b); // null
+
+
+		StringManipulator manipulator = new StringManipulator();
+		String word = manipulator.concatSubstring("Hello", 1, 2, "world");
+		System.out.println(word); // eworld
+
+
+	}
+}
