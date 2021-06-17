0cc4292c54569412f3114973764e7c16ffc615c9
Tom Wexler <wexler18@gmail.com>
2021-06-17 12:08:42 -0400

    need to study SLL more

---
+++ javaFun/singly-linked-list/SLLNode.java
@@ -0,0 +1,8 @@
+public class SLLNode {
+    public int value;
+    public SLLNode next;
+    public SLLNode(int val) {
+        this.value = val;
+        this.next = null;
+    }
+}
---
+++ javaFun/singly-linked-list/SLLTester.java
@@ -0,0 +1,13 @@
+public class SLLTester {
+    public static void main(String[] args) {
+        SinglyLinkedList sll = new SinglyLinkedList();
+
+        sll.AddBack(1);
+        sll.AddBack(2);
+        sll.AddBack(3);
+        sll.AddBack(4);
+
+        int val = sll.removeBack();
+        System.out.println(val);
+    }
+}
