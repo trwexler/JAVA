9435e60b33bd8e564f68fb3452a3fad47990bfa5
Tom Wexler <wexler18@gmail.com>
2021-06-21 18:58:07 -0400

    String assignment

---
+++ spring/firstproject/src/main/java/com/codingdojo/firstproject/FirstprojectApplication.java
@@ -0,0 +1,26 @@
+package com.codingdojo.firstproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class FirstprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstprojectApplication.class, args);
+	}
+	
+	@RequestMapping("/")
+	public String Hello() {
+		return "Hi, you're doing great!";
+	}
+	
+	@RequestMapping("/random")
+	public String Boot() {
+		return "Spring Boot Ruuuuulz!";
+	}
+
+}
